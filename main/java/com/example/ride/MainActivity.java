c7697646cf3d6f8b95fa2e37b75b22143fe11ea0
Md. Shakib Shahariar Junayed <71440395+itzjunayed@users.noreply.github.com>
2022-08-19 11:59:54 +0600

    Updated

--- main/java/com/example/ride/MainActivity.java
+++ main/java/com/example/ride/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.ride;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    AppCompatButton signupButton, loginButton, aboutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signupButton = findViewById(R.id.signup);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,signUp.class);
+                startActivity(intent);
+            }
+        });
+        loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,login.class);
+                startActivity(intent);
+            }
+        });
+        aboutButton = findViewById(R.id.about);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,about.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- main/java/com/example/ride/login.java
+++ main/java/com/example/ride/login.java
@@ -1,14 +1,40 @@
 package com.example.ride;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
-
+    AppCompatButton loginProfile;
+    EditText nameinput, passwordinput;
+    String name, pass
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        nameinput = findViewById(R.id.usernameSignup);
+
+        passwordinput = findViewById(R.id.passwordSignup);
+
+        loginProfile = findViewById(R.id.logintoprofile);
+        loginProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = nameinput.getText().toString();
+                email = emailinput.getText().toString();
+                nid = nidinput.getText().toString();
+                password = passwordinput.getText().toString();
+                User user = new User(name, email, password, nid);
+                Intent intent = new Intent(signUp.this,login.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
