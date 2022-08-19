53ab6919fea3ed8a2f186b99df73e1f4e0f1c09b
Md. Shakib Shahariar Junayed <71440395+itzjunayed@users.noreply.github.com>
2022-08-19 12:38:58 +0600

    Updated

--- main/java/com/example/ride/login.java
+++ main/java/com/example/ride/login.java
@@ -7,30 +7,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
-    AppCompatButton loginProfile;
+    AppCompatButton loginBtn;
+    TextView createText;
     EditText nameinput, passwordinput;
-    String name, pass
+    String name, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         nameinput = findViewById(R.id.usernameSignup);
-
         passwordinput = findViewById(R.id.passwordSignup);
 
-        loginProfile = findViewById(R.id.logintoprofile);
-        loginProfile.setOnClickListener(new View.OnClickListener() {
+        loginBtn = findViewById(R.id.logintoprofile);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                name = nameinput.getText().toString();
-                email = emailinput.getText().toString();
-                nid = nidinput.getText().toString();
-                password = passwordinput.getText().toString();
-                User user = new User(name, email, password, nid);
-                Intent intent = new Intent(signUp.this,login.class);
+                Intent intent = new Intent(login.this,home.class);
+                startActivity(intent);
+            }
+        });
+
+        createText = findViewById(R.id.signup);
+        createText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this,signUp.class);
                 startActivity(intent);
             }
         });
